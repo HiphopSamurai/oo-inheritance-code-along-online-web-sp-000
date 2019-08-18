69746b1014f4f7ae7344ff8d1735c8cae2b09c9a
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-18 00:53:02 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,11 @@
 require_relative "./vehicle.rb"
 
 class Car < Vehicle
+<<<<<<< HEAD
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
+=======
+
+>>>>>>> f85130a07dbdf104be57c1bb2e83006b2f439852
 end
